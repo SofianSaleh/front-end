20fe2dc250fb97367011314ab047bf4d0a2594bc
SofianSaleh <sofiane.saleh11@egmail.com>
2020-04-04 19:28:48 +0200

    apollo is hoked with react

---
+++ slack-clone/src/Routes/Home.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { gql } from "apollo-boost";
+import { useQuery } from '@apollo/react-hooks';
+
+
+const Home = () =>{
+    const { loading, error, data } = useQuery(allUsers)
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+
+    return data.getAllUsers.map(user => (
+        <div key={user.id}>
+            <h1>{user.username}</h1>
+        </div>
+    ))
+}
+
+const allUsers =  gql`
+    {
+        getAllUsers{
+            username
+        }
+    }
+`
+
+export default Home
+
--- slack-clone/src/Routes/index.js
+++ slack-clone/src/Routes/index.js
@@ -1,41 +1,12 @@
 import React from 'react';
-import ApolloClient from 'apollo-boost';
-import { gql } from "apollo-boost";
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-const client = new ApolloClient({
-    uri: 'https://localhost:8080/graphql',
-});
+import Home from './Home'
 
-client
-    .query({
-        query: gql`
-      {
-        getAllUsers() {
-          username
-        }
-      }
-    `
-    })
-    .then(result => console.log(result));
-
-function Index() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default Index;
+export default () => (
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" exact component={Home} />
+        </Switch>
+    </BrowserRouter>
+)
