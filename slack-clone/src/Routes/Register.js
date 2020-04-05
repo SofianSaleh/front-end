704938a04861335a4addf940729680ae68fad417
SofianSaleh <sofiane.saleh11@egmail.com>
2020-04-05 13:01:09 +0200

    finishing the log in

--- slack-clone/src/Routes/Register.js
+++ slack-clone/src/Routes/Register.js
@@ -1,67 +1,75 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
 
-import { gql } from "apollo-boost"
+import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
-
-import { Container, Input, Header, Button } from 'senematic-ui-react'
-
+import { graphql } from "@apollo/react-hoc";
+import { Container, Input, Header, Button } from "semantic-ui-react";
 
 class Register extends Component {
-    state = {
-        username: "",
-        email: "",
-        password: ""
-    }
+  state = {
+    username: "",
+    email: "",
+    password: ""
+  };
 
-    onChange(e) {
-        this.setState(({[e.target.name]:e.target.value}))
-    }
+  onChange(e) {
+    console.log({ [e.target.name]: e.target.value });
+    this.setState({ [e.target.name]: e.target.value });
+  }
 
-    onSubmit(e){
-        e.preventDefault()
-        const { data } = useQuery(registerQuery,{
-            variables: this.state
-        })
-        console.log(data)
-    }
+  onSubmit(e) {
+    e.preventDefault();
+    this.props
+      .mutate({
+        variables: this.state
+      })
+      .then(res => {
+        console.log(res);
+      });
+  }
 
-    render(){
-        const { username, email, password } = this.state
-        const { onChange, onSubmit } = this
-        return (
-            <Container text>
-                <Header as="h2">Register</Header>
-                <Input
-                    name={"username"}
-                    onChange={onChange}
-                    value={username}
-                    placeholder={"Username"}
-                    fluid/>
-                <Input
-                    name={"email"}
-                    onChange={onChange}
-                    value={email}
-                    placeholder={"Email"}
-                    fluid/>
-                <Input
-                    name={"password"}
-                    onChange={onChange}
-                    value={password}
-                    type="password"
-                    placeholder={"Password"}
-                    fluid/>
-                <Button onClick={onSubmit} primary>Submit</Button>
-            </Container>
-        )
-    }
+  render() {
+    const { username, email, password } = this.state;
+    return (
+      <Container text>
+        <Header as="h2">Register</Header>
+        <Input
+          name={"username"}
+          onChange={this.onChange.bind(this)}
+          value={username}
+          placeholder={"Username"}
+          fluid
+        />
+        <Input
+          name={"email"}
+          onChange={this.onChange.bind(this)}
+          value={email}
+          placeholder={"Email"}
+          fluid
+        />
+        <Input
+          name={"password"}
+          onChange={this.onChange.bind(this)}
+          value={password}
+          type="password"
+          placeholder={"Password"}
+          fluid
+        />
+        <Button onClick={this.onSubmit.bind(this)} primary>
+          Submit
+        </Button>
+      </Container>
+    );
+  }
 }
 
 const registerQuery = gql`
-mutation{
-  register($username: String!,$email: String!,$password: String!){
-    register(username: $username, email: $email, password: $password)
+  mutation register($username: String!, $email: String!, $password: String!) {
+    register(username: $username, email: $email, password: $password) {
+      message
+      success
+    }
   }
-}
-`
+`;
 
-export default Register
+export default graphql(registerQuery)(Register);
