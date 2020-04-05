b6f05e64b0624ea3e3a81b14b596e71b38ed65a4
SofianSaleh <sofiane.saleh11@egmail.com>
2020-04-05 14:40:14 +0200

    implemented redux and now converting

--- slack-clone/src/Routes/Register.js
+++ slack-clone/src/Routes/Register.js
@@ -1,39 +1,66 @@
 import React, { Component } from "react";
 
 import { gql } from "apollo-boost";
-import { useQuery } from "@apollo/react-hooks";
 import { graphql } from "@apollo/react-hoc";
-import { Container, Input, Header, Button } from "semantic-ui-react";
+import { Message, Container, Input, Header, Button } from "semantic-ui-react";
 
 class Register extends Component {
   state = {
     username: "",
+    usernameError: "",
     email: "",
-    password: ""
+    emailError: "",
+    password: "",
+    passwordError: ""
   };
 
-  onChange(e) {
-    console.log({ [e.target.name]: e.target.value });
+  onChange = e => {
     this.setState({ [e.target.name]: e.target.value });
-  }
+  };
 
-  onSubmit(e) {
+  onSubmit = async e => {
     e.preventDefault();
-    this.props
-      .mutate({
-        variables: this.state
-      })
-      .then(res => {
-        console.log(res);
+    this.setState({
+      usernameError: "",
+      emailError: "",
+      passwordError: ""
+    });
+    const { username, email, password } = this.state;
+    const response = await this.props.mutate({
+      variables: { username, email, password }
+    });
+    const { success, errors } = response.data.register;
+    if (success) {
+      this.props.history.push("/");
+    } else {
+      const err = {};
+      errors.forEach(({ path, message }) => {
+        err[`${path}Error`] = message;
       });
-  }
+      this.setState(err);
+    }
+  };
 
   render() {
-    const { username, email, password } = this.state;
+    const {
+      username,
+      email,
+      password,
+      usernameError,
+      emailError,
+      passwordError
+    } = this.state;
+
+    const errorsList = [];
+    if (usernameError) errorsList.push(usernameError);
+    if (emailError) errorsList.push(emailError);
+    if (passwordError) errorsList.push(passwordError);
+
     return (
       <Container text>
         <Header as="h2">Register</Header>
         <Input
+          error={usernameError}
           name={"username"}
           onChange={this.onChange.bind(this)}
           value={username}
@@ -41,6 +68,7 @@ class Register extends Component {
           fluid
         />
         <Input
+          error={emailError}
           name={"email"}
           onChange={this.onChange.bind(this)}
           value={email}
@@ -48,6 +76,7 @@ class Register extends Component {
           fluid
         />
         <Input
+          error={passwordError}
           name={"password"}
           onChange={this.onChange.bind(this)}
           value={password}
@@ -58,6 +87,13 @@ class Register extends Component {
         <Button onClick={this.onSubmit.bind(this)} primary>
           Submit
         </Button>
+        {usernameError || emailError || passwordError ? (
+          <Message
+            error
+            header="There was some errors in your submission"
+            list={errorsList}
+          />
+        ) : null}
       </Container>
     );
   }
@@ -66,8 +102,11 @@ class Register extends Component {
 const registerQuery = gql`
   mutation register($username: String!, $email: String!, $password: String!) {
     register(username: $username, email: $email, password: $password) {
-      message
       success
+      errors {
+        path
+        message
+      }
     }
   }
 `;
--- slack-clone/src/Routes/index.js
+++ slack-clone/src/Routes/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+import store from "../store";
+import { Provider } from "react-redux";
+
 import Home from "./Home";
 import Register from "./Register";
 
 export default () => (
-  <BrowserRouter>
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/register" exact component={Register} />
-    </Switch>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/register" exact component={Register} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>
 );
