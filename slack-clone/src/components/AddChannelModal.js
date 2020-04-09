f105d06c493f5fe6b4361bde3c430fb48b7c5edf
SofianSaleh <sofiane.saleh11@egmail.com>
2020-04-09 13:24:29 +0200

    modal is finished you can create channels

---
+++ slack-clone/src/components/AddChannelModal.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { Form, Input, Button, Modal } from "semantic-ui-react";
+import { gql } from "apollo-boost";
+import { graphql } from "@apollo/react-hoc";
+
+import { withFormik } from "formik";
+
+const AddChannelModal = ({
+  open,
+  onClose,
+  values,
+  handleChange,
+  handleBlur,
+  isSubmitting,
+  handleSubmit,
+}) => (
+  <Modal open={open} onClose={onClose}>
+    <Modal.Header>Add Channel</Modal.Header>
+    <Modal.Content>
+      <Form>
+        <Form.Field>
+          <Input
+            value={values.name}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            name="name"
+            fluid
+            placeholder="Channel name"
+          />
+        </Form.Field>
+        <Form.Group widths="equal">
+          <Button disabled={isSubmitting} fluid onClick={onClose}>
+            Cancel
+          </Button>
+          <Button disabled={isSubmitting} onClick={handleSubmit} fluid>
+            Create Channel
+          </Button>
+        </Form.Group>
+      </Form>
+    </Modal.Content>
+  </Modal>
+);
+
+const createChannelMutation = gql`
+  mutation($teamId: Int!, $name: String!) {
+    createChannel(teamId: $teamId, name: $name)
+  }
+`;
+
+export default graphql(createChannelMutation)(
+  withFormik({
+    mapPropsToValues: () => ({ name: "" }),
+    handleSubmit: async (
+      values,
+      { props: { onClose, teamId, mutate }, setSubmitting }
+    ) => {
+      const response = await mutate({
+        variables: { teamId, name: values.name },
+      });
+      console.log(response);
+      setSubmitting(false);
+      onClose();
+    },
+  })(AddChannelModal)
+);
--- slack-clone/src/containers/Sidebar.js
+++ slack-clone/src/containers/Sidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 import { gql } from "apollo-boost";
 import { graphql } from "@apollo/react-hoc";
@@ -8,44 +8,68 @@ import Teams from "../components/Teams";
 
 import _ from "lodash";
 import decode from "jwt-decode";
+import AddChannelModal from "../components/AddChannelModal";
 
-const Sidebar = ({ data: { loading, allTeams }, currentTeamId }) => {
-  if (loading) {
-    return null;
+class Sidebar extends Component {
+  state = {
+    openAddChannelModal: false,
+  };
+
+  handleCloseAddChannelModal = () => {
+    this.setState({ openAddChannelModal: false });
+  };
+
+  handleAddChannelClick = () => {
+    this.setState({ openAddChannelModal: true });
+  };
+  render() {
+    const {
+      data: { loading, allTeams },
+      currentTeamId,
+    } = this.props;
+    if (loading) {
+      return null;
+    }
+    const teamIdx = currentTeamId
+      ? _.findIndex(allTeams, ["id", parseInt(currentTeamId, 10)])
+      : 0;
+    const team = allTeams[teamIdx];
+    let username = "";
+
+    try {
+      const token = localStorage.getItem("token");
+      const { user } = decode(token);
+      username = user.username;
+    } catch (err) {}
+
+    return [
+      <Teams
+        key="team-sidebar"
+        teams={allTeams.map((t) => ({
+          id: t.id,
+          name: t.name.charAt(0).toUpperCase(),
+        }))}
+      />,
+      <Channels
+        key="channels-sidebar"
+        teamName={team.name}
+        username={username}
+        channels={team.channels}
+        users={[
+          { id: 1, name: "slackbot" },
+          { id: 2, name: "user1" },
+        ]}
+        onCreateChannel={this.handleAddChannelClick}
+      />,
+      <AddChannelModal
+        teamId={parseInt(currentTeamId)}
+        onClose={this.handleCloseAddChannelModal}
+        open={this.state.openAddChannelModal}
+        key="add-channel-modal-sidebar"
+      />,
+    ];
   }
-  console.log(allTeams);
-  const teamIdx = currentTeamId
-    ? _.findIndex(allTeams, ["id", parseInt(currentTeamId, 10)])
-    : 0;
-  const team = allTeams[teamIdx];
-  let username = "";
-
-  try {
-    const token = localStorage.getItem("token");
-    const { user } = decode(token);
-    username = user.username;
-  } catch (err) {}
-
-  return [
-    <Teams
-      key="team-sidebar"
-      teams={allTeams.map((t) => ({
-        id: t.id,
-        name: t.name.charAt(0).toUpperCase(),
-      }))}
-    />,
-    <Channels
-      key="channels-sidebar"
-      teamName={team.name}
-      username={username}
-      channels={team.channels}
-      users={[
-        { id: 1, name: "slackbot" },
-        { id: 2, name: "user1" },
-      ]}
-    />,
-  ];
-};
+}
 
 const allTeamsQuery = gql`
   {
